5f8187feb89d015cc032bec62f7e5e81132893d6
John Paul Morrison <jmorrison@bogomips.com>
2021-02-02 17:52:14 -0800

    vl805 binary/flash  and firmware check

---
+++ bin/vl805.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Version
+
+lspci -d 1106:3483 
+
+if [ "$USER" != "root" ] 
+then 
+  echo root needed to report version. use sudo 
+  exit 1
+fi
+
+lspci -d 1106:3483 -xxx | awk '/^50:/ { print "VL805 FW version: " $5 $4 $3 $2 }'
+
+
+
+
